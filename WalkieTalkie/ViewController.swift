e6f42c285c45b10dfe765fa5cf96dce66bc939fe
Rares <raresmarina@gmail.com>
2024-01-25 12:21:56 +0200

    Initial Commit

---
+++ WalkieTalkie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalkieTalkie
+//
+//  Created by Rares Marina on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
