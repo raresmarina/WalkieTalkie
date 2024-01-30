1025206a9253e651ee1d2d2a2ddea3109be08a93
Rares <raresmarina@gmail.com>
2024-01-30 18:17:03 +0200

    added: search users for new conversations

--- WalkieTalkie/Resources/DatabaseManager.swift
+++ WalkieTalkie/Resources/DatabaseManager.swift
@@ -82,13 +82,26 @@ extension DatabaseManager {
                     })
                 }
             })
-            
             completion(true)
         })
         
     }
+    
+    public func getAllUsers(completion: @escaping (Result<[[String: String]], Error>) -> Void) {
+        database.child("users").observeSingleEvent(of: .value, with: {snapshot in
+            guard let value = snapshot.value as?  [[String: String]] else {
+                completion(.failure(DatabaseError.failedToFetch))
+                return
+            }
+            completion(.success(value))
+        })
+    }
+    
+    public enum DatabaseError: Error{
+        case failedToFetch
+        
+    }
 }
-
 struct ChatAppUser {
     let firstName: String
     let lastName: String
