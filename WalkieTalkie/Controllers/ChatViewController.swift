6856dac22d755230062cd30e10a15c953eaa8dfa
Rares <raresmarina@gmail.com>
2024-01-31 15:54:25 +0200

    added: load messages in the database

--- WalkieTalkie/Controllers/ChatViewController.swift
+++ WalkieTalkie/Controllers/ChatViewController.swift
@@ -7,51 +7,145 @@
 
 import UIKit
 import MessageKit
+import InputBarAccessoryView
 
 struct Message: MessageType{
-    var sender: MessageKit.SenderType
-    var messageId: String
-    var sentDate: Date
-    var kind: MessageKit.MessageKind // supports text,photo,video,location...
+    public var sender: MessageKit.SenderType
+    public var messageId: String
+    public var sentDate: Date
+    public var kind: MessageKit.MessageKind // supports text,photo,video,location...
+}
+
+extension MessageKind {
+    var messageKindString: String{
+        switch self{
+            
+        case .text(_):
+            return "text"
+        case .attributedText(_):
+            return "attributed_text"
+        case .photo(_):
+            return "photo"
+        case .video(_):
+            return "video"
+        case .location(_):
+            return "location"
+        case .emoji(_):
+            return "emoji"
+        case .audio(_):
+            return "audio"
+        case .contact(_):
+            return "contact"
+        case .custom(_):
+            return "costum"
+        }
+    }
 }
 
 struct Sender: SenderType{
-    var photoURL: String
-    var senderId: String
-    var displayName: String
+    public var photoURL: String
+    public var senderId: String
+    public var displayName: String
 }
 class ChatViewController: MessagesViewController {
     
+    public static let dateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .long
+        formatter.locale = .current
+        return formatter
+    }()
+    
+    public var isNewConversation = false
+    public let otherUserEmail: String
     private var messages = [Message]()
-    private let selfSender = Sender(photoURL: "",
-                                senderId: "1",
-                                displayName: "Smith")
-
+    private var selfSender:Sender?  {
+        guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
+            return nil
+        }
+        return Sender(photoURL: "",
+                      senderId: email,
+                      displayName: "Smith")
+    }
+     init(with email: String) {
+        self.otherUserEmail = email
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        messages.append(Message(sender: selfSender,
-                                messageId: "1",
-                                sentDate: Date(),
-                                kind: .text("hello")))
-        messages.append(Message(sender: selfSender,
-                                messageId: "1",
-                                sentDate: Date(),
-                                kind: .text("hellohellohellohellohellohellohellohellohellohellohellohellohel")))
         view.backgroundColor = .red
         
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
-
+        messageInputBar.delegate = self
+        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        messageInputBar.inputTextView.becomeFirstResponder()
+    }
 }
-
+extension ChatViewController: InputBarAccessoryViewDelegate {
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        guard !text.replacingOccurrences(of: " ", with: "").isEmpty,
+        let selfSender = self.selfSender,
+        let messageId = createMessageId() else {
+            return
+        }
+        print("sending: \(text)")
+        
+        //Send message
+        if isNewConversation{
+            //create conversation
+            let message = Message(sender: selfSender,
+                                  messageId: messageId,
+                                  sentDate: Date(),
+                                  kind: .text(text))
+            DatabaseManager.shared.createNewConversation(with: otherUserEmail, firstMessage: message, completion: { success in
+                if success {
+                    print("message sent")
+                } else {
+                    print("failed to send")
+                    }
+            })
+        }
+        else{
+            //append to existing conversation data
+            
+        }
+        
+    }
+    private func createMessageId() -> String? {
+        ///date, otherUserEmail, senderEmail, randomInt
+        
+        guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String else {
+            return nil
+        }
+        
+        let safeCurrentEmail = DatabaseManager.safeEmail(emailAddress: currentUserEmail)
+        let dateString = Self.dateFormatter.string(from: Date())
+        let newIdentifier = " \(otherUserEmail)_\(safeCurrentEmail)_\(dateString)"
+        
+        return newIdentifier
+    }
+}
 extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate{
     func currentSender() -> MessageKit.SenderType {
         //decide who send the message
-        return selfSender
+        if let sender = selfSender{
+            return sender
+        }
+        fatalError("Self sender is nil, email should be cached")
+        return Sender(photoURL: "", senderId: "12", displayName: "")
     }
     
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
--- WalkieTalkie/Controllers/ConversationsViewController.swift
+++ WalkieTalkie/Controllers/ConversationsViewController.swift
@@ -42,8 +42,23 @@ class ConversationsViewController: UIViewController {
     @objc private func didTapComposeButton(){
         
         let vc = NewConversationViewController()
+        vc.completion = {[weak self]result in
+            self?.createNewConversation(result: result)
+        }
         let navVC = UINavigationController(rootViewController: vc)
         present(navVC,animated: true)
+        
+    }
+    
+    private func createNewConversation(result: [String:String]){
+        guard let name = result["name"], let email = result["email"] else {
+            return
+        }
+        let vc = ChatViewController(with: email)
+        vc.isNewConversation = true
+        vc.title = name
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
         
     }
     
@@ -93,7 +108,7 @@ extension ConversationsViewController: UITableViewDelegate, UITableViewDataSourc
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let vc = ChatViewController()
+        let vc = ChatViewController(with: "bla@gmail.com ")
         vc.title = "Jenny"
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
